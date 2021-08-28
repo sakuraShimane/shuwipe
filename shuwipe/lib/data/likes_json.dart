acf69bc28aad8d96b1b3f3bc947c1d8214694bfe
sakura <m.suuuiiiii39@gmail.com>
2021-08-28 15:02:55 +0900

    Shuwipe　コード凍結！

--- shuwipe/lib/data/likes_json.dart
+++
@@ -1,6 +0,0 @@
-const List likes_json = [
-  {"img": "assets/images/human/hr05.jpg", "active": true},
-  {"img": "assets/images/human/hr01.jpeg", "active": true},
-  {"img": "assets/images/human/hr07.jpeg", "active": false},
-  {"img": "assets/images/human/hr06.jpeg", "active": false},
-];
